2eb2d3f6bc312418bbfebe1cae5ed5d39e5a13b0
david-gonzalez15 <65929977+david-gonzalez15@users.noreply.github.com>
2021-06-11 10:59:30 -0500

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBUtTIZyt9dfh-mMZ1oBEJ4B2k_5BqI8Zc",
+    authDomain: "odyssey-c2561.firebaseapp.com",
+    projectId: "odyssey-c2561",
+    storageBucket: "odyssey-c2561.appspot.com",
+    messagingSenderId: "318698140601",
+    appId: "1:318698140601:web:3a2ff8d71e206316e0fc68",
+    measurementId: "G-4LPPF0DQCZ"
+  };
+//--
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export {db, auth}
